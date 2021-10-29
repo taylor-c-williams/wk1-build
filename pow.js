a9f2de60e9ad82533d241bd0d0a454aed014b33b
Taylor <taylor.callan.williams@gmail.com>
2021-10-29 11:40:35 -0700

    removed comments

--- pow.js
+++ pow.js
@@ -1,5 +1,3 @@
-// In this build you will use the Red, Green, Refactor approach to create a function that takes a base and exponent and raises the base to the exponent power:
-// base<sup>exponent</sup>.
 const pow = (base, exponent) => {
 const solution = base ** exponent
 return solution
