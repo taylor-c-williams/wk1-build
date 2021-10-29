c7b00ad7353ca37a96ce6f2bc2be5a761f93e519
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-29 10:10:30 -0700

    math.pow version written and passing

--- pow.js
+++ pow.js
@@ -1,8 +1,8 @@
 // In this build you will use the Red, Green, Refactor approach to create a function that takes a base and exponent and raises the base to the exponent power:
 // base<sup>exponent</sup>.
 const pow = (base, exponent) => {
-Math.pow(base, exponent)
-return pow
+const solution = Math.pow(base, exponent)
+return solution
 };
 module.exports = pow;
   
