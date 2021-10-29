42cd7f28b59fe982bd6173bf7ceb3a20e7cc6bf1
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-29 10:12:20 -0700

    refactored with exponentiation operator

--- pow.js
+++ pow.js
@@ -1,7 +1,7 @@
 // In this build you will use the Red, Green, Refactor approach to create a function that takes a base and exponent and raises the base to the exponent power:
 // base<sup>exponent</sup>.
 const pow = (base, exponent) => {
-const solution = Math.pow(base, exponent)
+const solution = base ** exponent
 return solution
 };
 module.exports = pow;
